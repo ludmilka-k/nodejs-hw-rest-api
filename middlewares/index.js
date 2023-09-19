218ddb68c0d6cac380aa7f006c9f78e7bbce7cc7
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-09-19 12:04:15 +0300

    Update helpers

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,2 +1,3 @@
 export { default as validatedContacts } from "./validatedContacts.js";
 export { default as isValidId } from "./isValidId.js";
+export { default as authenticate} from "./authenticate.js";
